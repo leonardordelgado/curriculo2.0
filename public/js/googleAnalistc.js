5b0d6157416315ea6179bed08a2e57c41e328e95
leonardordelgado <leonardoresende311015@gmail.com>
2023-04-22 22:18:32 -0300

    google'

--- public/js/googleAnalistc.js
+++ public/js/googleAnalistc.js
@@ -1,5 +1,5 @@
 window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-gtag('config', 'G-JN9LM1KPMV');
+  gtag('config', 'G-JN9LM1KPMV');
